1e1e4a4e3b4ee38bee66e5d7c744af3e5222ec42
sarthak <skothia07@gmail.com>
2020-09-14 15:04:10 +0530

    smoke

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gameState=1
 
 
 function preload() {
@@ -69,12 +70,14 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState===1){
+       Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
-
 
 function mouseReleased(){
     slingshot.fly();
+    gameState=0
 }
 
 function keyPressed(){
